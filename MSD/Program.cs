d8f00d845ff4b4bf8b9c733c5c54b328db48be05
marushchak22 <marusaknazar0@gmail.com>
2023-05-30 18:51:46 +0300

    LSD, MSD algoritm

---
+++ MSD/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Family
+{
+    public List<int> Treasures { get; set; }
+}
+
+class TA12
+{
+    static int getMax(List<int> arr)
+    {
+        int mx = arr[0];
+        foreach (int num in arr)
+        {
+            if (num > mx)
+                mx = num;
+        }
+        return mx;
+    }
+
+    static void countSort(List<int> arr, int exp)
+    {
+        int n = arr.Count;
+        List<int> output = new List<int>(new int[n]);
+        int[] count = new int[10];
+
+        for (int i = 0; i < 10; i++)
+            count[i] = 0;
+
+        foreach (int num in arr)
+            count[(num / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    static void radixsort(List<int> arr)
+    {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10)
+            countSort(arr, exp);
+    }
+
+    static void Main()
+    {
+        List<Family> families = new List<Family>
+        {
+            new Family { Treasures = new List<int> { 1000 } },
+            new Family { Treasures = new List<int> { 1500} },
+            new Family { Treasures = new List<int> { 900} },
+            new Family { Treasures = new List<int> { 6 } },
+            new Family { Treasures = new List<int> { 100} },
+            new Family { Treasures = new List<int> { 1500 } },
+            new Family { Treasures = new List<int> { 100 } },
+            new Family { Treasures = new List<int> { 500} },
+            new Family { Treasures = new List<int> { 9000} },
+            new Family { Treasures = new List<int> { 699} },
+            new Family { Treasures = new List<int> { 100} },
+            new Family { Treasures = new List<int> { 0 } }
+        };
+
+        List<int> allTreasures = new List<int>();
+
+        foreach (Family family in families)
+            allTreasures.AddRange(family.Treasures);
+
+        Console.WriteLine("Список сумарних скарбів родин:");
+
+        radixsort(allTreasures);
+
+        foreach (int treasure in allTreasures)
+            Console.WriteLine(treasure);
+
+        Console.ReadLine();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var familiesList = new List<Family>
+        {
+            new Family { Treasures = new List<int> { 1000, 500, 200, 300 } },
+            new Family { Treasures = new List<int> { 1500, 800, 1200 } },
+            new Family { Treasures = new List<int> { 900, 2000, 3000, 500 } },
+            new Family { Treasures = new List<int> { 600, 117600, 1700 } },
+            new Family { Treasures = new List<int> { 1000, 500, 200, 300 } },
+            new Family { Treasures = new List<int> { 1500, 800, 120460 } },
+            new Family { Treasures = new List<int> { 900, 2000, 3000, 500 } },
+            new Family { Treasures = new List<int> { 600, 1100, 1750 } },
+            new Family { Treasures = new List<int> { 1000, 500, 200, 38800 } },
+            new Family { Treasures = new List<int> { 1500, 800, 1200 } },
+            new Family { Treasures = new List<int> { 900, 24000, 3000, 5005 } },
+            new Family { Treasures = new List<int> { 600, 1100, 1700 } },
+            new Family { Treasures = new List<int> { 10, 500, 2300, 3 } },
+            new Family { Treasures = new List<int> { 1500, 800, 1200 } },
+            new Family { Treasures = new List<int> { 900, 2000, 3000, 500 } },
+            new Family { Treasures = new List<int> { 600, 1, 10 } },
+        };
+
+        var totalTreasures = new List<int>();
+
+        foreach (var family in familiesList)
+        {
+            var sum = family.Treasures.Sum();
+            totalTreasures.Add(sum);
+        }
+
+        LSDSort(totalTreasures);
+
+        Console.WriteLine("Список сумарних скарбів родин:");
+        foreach (var treasure in totalTreasures)
+        {
+            Console.WriteLine(treasure);
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine();
+        var totalSum = totalTreasures.Sum();
+        Console.WriteLine("Сума всіх скарбів родин разом: " + totalSum);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    static void LSDSort(List<int> numbers)
+    {
+        if (numbers.Count <= 1)
+            return;
+
+        int maxDigits = numbers.Max().ToString().Length;
+        int[] output = new int[numbers.Count];
+
+        for (int digit = 0; digit < maxDigits; digit++)
+        {
+            int[] count = new int[10];
+
+            foreach (int number in numbers)
+            {
+                int digitValue = GetDigitValue(number, digit);
+                count[digitValue]++;
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                int number = numbers[i];
+                int digitValue = GetDigitValue(number, digit);
+                count[digitValue]--;
+                output[count[digitValue]] = number;
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] = output[i];
+            }
+        }
+    }
+
+    static int GetDigitValue(int number, int digit)
+    {
+        return (number / (int)Math.Pow(10, digit)) % 10;
+    }
+}
+
+class Family
+{
+    public List<int> Treasures { get; set; }
+}
